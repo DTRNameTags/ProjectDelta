1ce9bb58f31702390a4853d4a2c7a83b91a5c1ab
DTRNameTags <galacticnodes@gmail.com>
2023-07-11 07:22:36 -0500

    added

---
+++ PD-Server/Server/Controllers/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharedLibrary;
+
+namespace Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PlayerController : ControllerBase {
+
+    [HttpGet]
+    public Player Get([FromQuery] int id) {
+        var player = new Player();
+
+        return player;
+    }
+
+    [HttpPost]
+    public Player Post(Player player) {
+        Console.WriteLine("Player Has Been Added To The Database");
+
+        return player;
+    }
+
+}
